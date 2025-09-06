758b70ea7061fabc2e14a05f69225dd1ebb414f0
Kim Benedict N. Sumilang <kimbenedict6@gmail.com>
2025-09-06 16:06:44 +0800

    Create Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sdca_it_elec4_flutter/screens/home_screen.dart';
 
 void main() {
   runApp(App());
@@ -6,23 +7,13 @@ void main() {
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    Scaffold homepage = Scaffold(
-      appBar: AppBar(title: Text('Homepage')),
-      floatingActionButton: FloatingActionButton(
-        onPressed:() {
-           // TODO: Implement button 
-        },
-        child: const Icon(Icons.add)
-        ), 
-    );
-
+  Widget build(BuildContext context) {   
     return MaterialApp(
-      home: homepage,
+      home: HomeScreen(),
       theme: ThemeData(
         appBarTheme: AppBarTheme(
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.white
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+          foregroundColor: const Color.fromARGB(255, 221, 70, 0)
         )
       ),
     );
